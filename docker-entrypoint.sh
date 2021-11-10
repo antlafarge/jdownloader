9587a83189eb0e4c99b46787b6e0bfa908cf6c29
Antoine Lafarge <ant.lafarge@gmail.com>
2021-11-10 04:12:10 +0100

    Improve logs

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -80,12 +80,14 @@ then
 
     if [ $curlExitCode -ne 0 ]
     then
+        log "WARNING" "$JDownloaderJarFile download failed: curl exited with code '$curlExitCode'"
+
         wget "$JDownloaderJarUrl" 2> /dev/null
         wgetExitCode=$?
 
         if [ $wgetExitCode -ne 0 ]
         then
-            fatal "$JDownloaderJarFile download failed: curl exited with code '$curlExitCode' and wget exited with code '$wgetExitCode'"
+            fatal "$JDownloaderJarFile download failed: wget exited with code '$wgetExitCode'"
         fi
     fi
 fi
