e92d0f5688aa3a5ec66e514b2fbb7f8fca160d5f
Jan <loeffelpan@erdmail.de>
2023-05-28 11:41:00 +0000

    Using jq instead of custom sed-function for modifying json files

--- setup.sh
+++ setup.sh
@@ -12,30 +12,6 @@ email=$1
 password=$2
 devicename=$3
 
-# Replace a JSON value by searching its field in a JSON file
-# Usage : replaceJsonValue file field value
-replaceJsonValue()
-{
-    file=$1
-    field=$(printf "%s" "$2" | sed -e 's/\\/\\\\/g' -e 's/[]\/$*.^[]/\\&/g') # this field will be compared to a value from a json file, so we need to double escape the backslashes \\\\    And this field will be used in a sed regex, so we escape regex special characters ]\/$*.^[
-    newValue=$(printf "%s" "$3" | sed -e 's/[\/&]/\\&/g' -e 's/"/\\\\"/g') # this value will be used in a sed replace, so we escape replace special characters \/&    And this value will be stored in a json file, so finally we double escape the quotes \\\\"
-
-    fieldPart="\($field\)" # match the field
-    valuePart="\([^\\\"]\|\\\\.\)*" # match the value. This looks complicated because it can contain escaped quotes \" because of json format.
-
-    search="\"$fieldPart\"\s*:\s*\"$valuePart\""
-    replace="\"\1\":\"$newValue\""
-
-    sed -i "s/$search/$replace/g" $file
-    sedExitCode=$?
-
-    if [ $sedExitCode -ne 0 ]
-    then
-        log "ERROR" "sed exited with code '$sedExitCode'"
-        exit $sedExitCode
-    fi
-}
-
 cfgDir="./cfg/"
 
 # If JDownloader cfg directory does not exist
@@ -82,17 +58,17 @@ fi
 if [ -n "$email" ]
 then
     log "Replace JDownloader email in myJDownloader settings file"
-    replaceJsonValue $myJDownloaderSettingsFile "email" "$email"
+    jq --arg email $email '.email = $email' $myJDownloaderSettingsFile > temp.json && mv temp.json $myJDownloaderSettingsFile
 fi
 
 if [ -n "$password" ]
 then
     log "Replace JDownloader password in myJDownloader settings file"
-    replaceJsonValue $myJDownloaderSettingsFile "password" "$password"
+    jq --arg password $password '.password = $password' $myJDownloaderSettingsFile > temp.json && mv temp.json $myJDownloaderSettingsFile
 fi
 
 if [ -n "$devicename" ]
 then
     log "Replace JDownloader devicename in myJDownloader settings file"
-    replaceJsonValue $myJDownloaderSettingsFile "devicename" "$devicename"
+    jq --arg devicename $devicename '.devicename = $devicename' $myJDownloaderSettingsFile > temp.json && mv temp.json $myJDownloaderSettingsFile
 fi
